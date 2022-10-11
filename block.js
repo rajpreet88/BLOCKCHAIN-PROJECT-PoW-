3953e1098ba8405ac5b67935763fdcba44c8528d
RajPreet <sahupreetamkumar@gmail.com>
2022-10-11 13:27:22 +0530

    Created Block

--- block.js
+++ block.js
@@ -1,11 +1,19 @@
 class Block{
-    constructor(timestamp, prevHash, hash, data){
+    constructor({timestamp, prevHash, hash, data}){
         this.timestamp = timestamp;
         this.prevHash = prevHash;
         this.hash = hash;
-        this.data= data;
+        this.data = data;
     }
 }
 
-//creating object of the Block Class to call the constructor initially
-const block1 = new Block("11/10/22","0xacb","0xc12","Hello");
+//creating object of the Block Class to call the constructor
+const block1 = new Block(
+    {
+        timestamp: "11/10/22",
+        prevHash: "0xacb",
+        hash: "0xc12",
+        data: "Hello World"
+    });
+// const block2 = new Block("11/10/22","0xc12","0x123","World");
+console.log(block1);
