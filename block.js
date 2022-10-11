7ea37890ea59e84bd846a8b773b9bf31b4f8c49a
RajPreet <sahupreetamkumar@gmail.com>
2022-10-11 14:43:09 +0530

    created Genesis Black and called it in the Block.js

--- block.js
+++ block.js
@@ -1,9 +1,16 @@
+const {GENESIS_BLOCK} = require('./config');
+
 class Block{
     constructor({timestamp, prevHash, hash, data}){
         this.timestamp = timestamp;
         this.prevHash = prevHash;
         this.hash = hash;
         this.data = data;
+    }
+
+    //this static function is used to prevent the function from being everytime there is a new object created 
+    static genesis(){
+        return new this(GENESIS_BLOCK);
     }
 }
 
@@ -16,4 +23,6 @@ const block1 = new Block(
         data: "Hello World"
     });
 // const block2 = new Block("11/10/22","0xc12","0x123","World");
-console.log(block1);
+// console.log(block1);
+const genesisBlock = Block.genesis();
+console.log(genesisBlock)
