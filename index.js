b9326b7888822b17e42c42e54f3aa098394edd5b
RajPreet <sahupreetamkumar@gmail.com>
2022-11-05 20:49:55 +0530

    generated dyanamic peer-ports

--- index.js
+++ index.js
@@ -2,16 +2,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const Blockchain = require("./blockchain");
-const PubSub = require('./publish-subscribe');
-const PORT = 8000;
+const PubSub = require("./publish-subscribe");
+
+const DEFAULT_PORT = 8000;
+let PEER_PORT;
+
 const app = express();
 
 //blockchain object
 const blockchain = new Blockchain();
-const pubsub = new PubSub({blockchain});
+const pubsub = new PubSub({ blockchain });
 
 //broadcast the chain to all node available in the network
-setTimeout(()=> pubsub.broadcastChain(),1000);
+setTimeout(() => pubsub.broadcastChain(), 1000);
 
 //built-in middleware to accept post dat in JSON in expressJs
 app.use(bodyParser.json());
@@ -22,10 +25,17 @@ app.get("/api/blocks", (req, res) => {
 
 app.post("/api/mine", (req, res) => {
   const { data } = req.body;
-
-  blockchain.addBlock({ data });
+  blockchain.addBlock({ data }); //add block after validation and then broadcast for all the node isn the network
+  pubsub.broadcastChain();
   res.status(200).redirect("/api/blocks");
 });
+
+//dynamically generate peer ports
+if (process.env.GENERATE_PEER_PORT === "true") {
+  PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
+}
+
+let PORT = PEER_PORT || DEFAULT_PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port: ${PORT}`);
