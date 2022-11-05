ab2266d76b32e49389e5003a8482b82996840c0b
RajPreet <sahupreetamkumar@gmail.com>
2022-11-05 10:48:57 +0530

    installed nodemon and body-parser

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //<npm i express --save>
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
 const PORT = 8000;
 const app = express();
@@ -7,8 +8,18 @@ const app = express();
 //blockchain object
 const blockchain = new Blockchain();
 
+//built-in middleware to accept post dat in JSON in expressJs
+app.use(bodyParser.json());
+
 app.get('/api/blocks',(req,res)=>{
     res.json(blockchain.chain)
+})
+
+app.post('api/mine',(req,res)=>{
+    const {data} = req.body;
+
+    blockchain.addBlock({data});
+    res.status(200).redirect('api/blocks');
 })
 
 app.listen(PORT,()=>{
